837e7aa8e7954462028785154f1b0e2765b5c47b
Seikh B. Islam <seikh.b.islam@gmail.com>
2014-06-05 17:00:21 -0400

    Submitting Assignment

--- plot4.R
+++ plot4.R
@@ -14,25 +14,25 @@ data_reqd <- data_all[(data_all$Date>="2007-02-01") & (data_all$Date<="2007-02-0
 png(filename="plot4.png", width=480, height=480, units="px")
 par(mfrow=c(2,2))
 
-#1st 
-plot(data_reqd$Global_active_power, type="l",xaxt="n",xlab="", ylab="Global Active Power")
+#1st sub-plot
+plot(data_reqd$Global_active_power, type="l", xaxt="n", xlab="", ylab="Global Active Power")
 axis(1, at=c(1, as.integer(nrow(data_reqd)/2), nrow(data_reqd)), labels=c("Thu", "Fri", "Sat"))
 
-#2nd
-plot(data_reqd$Voltage, type="l",xaxt="n",xlab="datetime", ylab="Voltage")
+#2nd sub-plot
+plot(data_reqd$Voltage, type="l", xaxt="n", xlab="datetime", ylab="Voltage")
 axis(1, at=c(1, as.integer(nrow(data_reqd)/2), nrow(data_reqd)), labels=c("Thu", "Fri", "Sat"))
 
-#3rd
+#3rd sub-plot
 with(data_reqd, {
-    plot(Sub_metering_1,type="l", xaxt="n", xlab="", ylab="Energy sub metering")
+    plot(Sub_metering_1, type="l", xaxt="n", xlab="", ylab="Energy sub metering")
     lines(x=Sub_metering_2, col="red")
     lines(x=Sub_metering_3, col="blue")
 })
 axis(1, at=c(1, as.integer(nrow(data_reqd)/2), nrow(data_reqd)), labels=c("Thu", "Fri", "Sat"))
 legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), box.lwd = 0,box.col = "transparent", bg="transparent")
 
-#4th
-plot(data_reqd$Global_reactive_power, type="l",xaxt="n",xlab="datetime", ylab="Global_reactive_power")
+#4th sub-plot
+plot(data_reqd$Global_reactive_power, type="l", xaxt="n", xlab="datetime", ylab="Global_reactive_power")
 axis(1, at=c(1, as.integer(nrow(data_reqd)/2), nrow(data_reqd)), labels=c("Thu", "Fri", "Sat"))
 
 dev.off()
